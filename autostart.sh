ec8ddb64d5b67caa1d4efedbf3b6d522119b8030
safin-sys <mdsafinofficial@gmail.com>
2022-02-28 02:35:06 +0600

    weather added on status

--- autostart.sh
+++ autostart.sh
@@ -2,8 +2,17 @@
 
 nitrogen --restore &
 
-DATE=$(date +"%a %d-%b-%y %_I:%M%P")
+dte(){
+  dte=$(date +"%_I:%M%P | %a %d-%b-%y")
+  echo "$dte"
+}
+
+wth(){
+  wth=$(curl -s "wttr.in/Dhaka?format=1" | sed "s/[^0-9A-Z°]//g")
+  echo "$wth"
+}
+
 while true; do
-	xsetroot -name "$DATE"
+	xsetroot -name " $(wth) | $(dte) "
 	sleep 1m
 done &
